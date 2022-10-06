969a94d73dacf03cd177c043b4e8b8829082300c
GaboJHSP <104600151+GaboJHSP@users.noreply.github.com>
2022-10-06 11:34:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clase_Vue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-jtd9s2)
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://i.kym-cdn.com/photos/images/newsfeed/001/942/590/665.jpg">
+    <HelloWorld msg="Hola Mundo"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
